7b9f7e6f41b7809ab42e0e455e8611fc07c965e8
Rishikesh Mane <geeksforrex@gmail.com>
2021-05-06 18:39:28 +0530

    Update volounteer.js

--- routes/volounteer.js
+++ routes/volounteer.js
@@ -68,7 +68,7 @@ router.post('/', upload.single('image'), async (req, res) => {
     });
 
     //send email
-
+    //changes to make
     var transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -78,8 +78,8 @@ router.post('/', upload.single('image'), async (req, res) => {
     });
     
     var mailOptions = {
-      from: 'rexspecsadam007@gmail.com',
-      to: 'geeksforrex@gmail.com',
+      from: '',
+      to: '',
       subject: 'Sending Email using Node.js',
       text: `Hi Smartherd, thank you for your nice Node.js tutorials.
               I will donate 50$ for this course. Please send me payment options.`
